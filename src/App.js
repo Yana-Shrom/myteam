075d22d31ebb443c1dc1c7c57c0a45153cdb5a68
Yana-Shrom <dertilemene15@gmail.com>
2025-04-04 10:49:20 +0200

    mise en place webrtc

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { useRef, useState } from 'react';
 
 function App() {
+
+  const videoRef = useRef(null);
+  const [isStreaming, setIsStreaming] = useState(false);
+  const constraints = {
+    'video': true,
+    'audio': true
+  }
+  const startCamera = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia(constraints);
+      if( videoRef.current){
+        videoRef.current.srcObjet = stream;
+      }
+      setIsStreaming(true);
+    } catch(err){
+      console.error("Error");
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={startCamera} disabled={isStreaming}>
+        {isStreaming ? "Caméra activée" : "Démarrer la caméra"}
+      </button>
+      <video ref={videoRef} autoPlay style={{width:"640px", height: "480px", backgroundColor:"#000"}}></video>
     </div>
   );
 }
