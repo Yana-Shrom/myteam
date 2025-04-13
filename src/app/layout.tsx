20a202ce2c6eb9548b3acc1b2cbe447295d959f4
Yana-Shrom <dertilemene15@gmail.com>
2025-04-13 18:38:15 +0200

    ajout de la mise en forme avec librairie ShadCN UI et definission de la couleur principale

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "../components/app-sidebar"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,10 +26,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      <body>
+        
+      <SidebarProvider
+        style={{
+          "--sidebar-width": "5rem",
+          "--sidebar-width-mobile": "5rem",
+        } as React.CSSProperties
+      }
       >
-        {children}
+        <AppSidebar />
+        <main>
+          {children}
+        </main>
+      </SidebarProvider>
       </body>
     </html>
   );
